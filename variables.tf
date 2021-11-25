e6c6b40ca1c257e94501411dbeb59fea1c7f70b6
Guo <GGuo@C02CT3HXMD6P-MBP.globeandmail.net>
2021-11-25 13:43:36 -0500

    DSOPS-3:fix:remove unused TF variable svcs_account_virginia_kms_cmk_arn_for_secrets_manager

--- variables.tf
+++ variables.tf
@@ -8,10 +8,9 @@ variable "deploy_type" { #reserved
   description = "(Required) Must be one of the following ( ecr, ecs, lambda )."
 }
 
-variable "ecr_name" { #reserved
-  type        = string
-  description = "(Optional) The name of the ECR repo. Required if var.deploy_type is ecr or ecs."
-  default     = null
+variable "aws_organization_id" {
+  type         = string
+  description  = "(Required) The AWS organization ID."
 }
 
 variable "codebuild_image" { #reserved
@@ -26,16 +25,16 @@ variable "build_compute_type" { #reserved
   default     = "BUILD_GENERAL1_SMALL"
 }
 
+variable "buildspec" { #reserved
+  type        = string
+  description = "(Optional) The name of the buildspec file to use with codebuild. Defaults to buildspec.yml."
+  default     = "buildspec.yml"
+}
+
 variable "logs_retention_in_days" { #reserved
   type        = number
   description = "(Optional) Days to keep the cloudwatch logs for the codebuild project. Defaults to 14."
   default     = 14
-}
-
-variable "privileged_mode" { #reserved
-  type        = bool
-  description = "(Optional) Use privileged mode for docker containers. Defaults to false."
-  default     = false
 }
 
 variable "tags" { #reserved
@@ -65,48 +64,10 @@ variable "github_oauth_token" { #reserved
   description = "(Required) The GitHub oauth token."
 }
 
-variable "lambda_function_name" { #reserved
-  type        = string
-  description = "(Optional) The name of the lambda function to update. Required if var.deploy_type is lambda."
-  default     = null
-}
-
 variable "create_github_webhook" { #reserved
   type        = bool
   description = "(Optional) Create the github webhook that triggers codepipeline. Defaults to true."
   default     = true
-}
-
-variable "use_docker_credentials" { #reserved
-  type        = bool
-  description = "(Optional) Use dockerhub credentals stored in parameter store. Defaults to false."
-  default     = false
-}
-
-variable "buildspec" { #reserved
-  type        = string
-  description = "(Optional) The name of the buildspec file to use with codebuild. Defaults to buildspec.yml."
-  default     = "buildspec.yml"
-}
-
-variable "svcs_account_github_token_aws_secret_arn" { #reserved
-  type        = string
-  description = <<EOT
-                (Optional) The AWS secret ARN for the repo access Github token.
-                The secret is created in the shared service account.
-                Required if var.use_repo_access_github_token is true.
-                EOT
-  default     = null
-}
-
-variable "svcs_account_github_token_aws_kms_cmk_arn" { #reserved
-  type        = string
-  description = <<EOT
-                (Optional) The us-east-1 region AWS KMS customer managered key ARN for encrypting the repo access Github token AWS secret.
-                The key is created in the shared service account.
-                Required if var.use_repo_access_github_token is true.
-                EOT
-  default     = null
 }
 
 variable "non_default_aws_provider_configurations" {
@@ -122,13 +83,18 @@ variable "non_default_aws_provider_configurations" {
   default = {}
 }
 
-variable "use_repo_access_github_token" {
+variable "lambda_function_name" { #reserved
+  type        = string
+  description = "(Optional) The name of the lambda function to update. Required if var.deploy_type is lambda."
+  default     = null
+}
+
+variable "s3_bucket_force_destroy" {
   type        = bool
   description = <<EOT
-                (Optional) Allow the AWS codebuild IAM role read access to the REPO_ACCESS_GITHUB_TOKEN secrets manager secret in the shared service account.
-                Defaults to false.
+                (Required) Delete all objects in S3 bucket upon bucket deletion. S3 objects are not recoverable.
+                Set to true if var.deploy_type is ecs or lambda.
                 EOT
-  default     = false
 }
 
 variable "create_cross_region_resources" {
@@ -147,12 +113,12 @@ variable "create_ireland_region_resources" {
                 EOT
 }
 
-variable "svcs_account_virginia_kms_cmk_arn_for_secrets_manager" {
+variable "svcs_account_ireland_kms_cmk_arn_for_s3" {
   type        = string
   description = <<EOT
-                (Optional) The us-east-1 region AWS KMS customer managed key ARN for encrypting secrets manager data.
+                (Optional) The eu-west-1 region AWS KMS customer managed key ARN for encrypting s3 data.
                 The key is created in the shared service account.
-                Required if var.use_repo_access_github_token is true.
+                Required if var.create_ireland_region_resources is true.
                 EOT
   default     = null
 }
@@ -167,25 +133,49 @@ variable "svcs_account_virginia_kms_cmk_arn_for_s3" {
   default     = null
 }
 
-variable "svcs_account_ireland_kms_cmk_arn_for_s3" {
+variable "ecr_name" { #reserved
+  type        = string
+  description = "(Optional) The name of the ECR repo. Required if var.deploy_type is ecr or ecs."
+  default     = null
+}
+
+variable "privileged_mode" { #reserved
+  type        = bool
+  description = "(Optional) Use privileged mode for docker containers. Defaults to false."
+  default     = false
+}
+
+variable "use_docker_credentials" { #reserved
+  type        = bool
+  description = "(Optional) Use dockerhub credentals stored in parameter store. Defaults to false."
+  default     = false
+}
+
+variable "use_repo_access_github_token" {
+  type        = bool
+  description = <<EOT
+                (Optional) Allow the AWS codebuild IAM role read access to the REPO_ACCESS_GITHUB_TOKEN secrets manager secret in the shared service account.
+                Defaults to false.
+                EOT
+  default     = false
+}
+
+variable "svcs_account_github_token_aws_secret_arn" { #reserved
   type        = string
   description = <<EOT
-                (Optional) The eu-west-1 region AWS KMS customer managed key ARN for encrypting s3 data.
-                The key is created in the shared service account.
-                Required if var.create_ireland_region_resources is true.
+                (Optional) The AWS secret ARN for the repo access Github token.
+                The secret is created in the shared service account.
+                Required if var.use_repo_access_github_token is true.
                 EOT
   default     = null
 }
 
-variable "s3_bucket_force_destroy" {
-  type        = bool
+variable "svcs_account_github_token_aws_kms_cmk_arn" { #reserved
+  type        = string
   description = <<EOT
-                (Required) Delete all objects in S3 bucket upon bucket deletion. S3 objects are not recoverable.
-                Set to true if var.deploy_type is ecs or lambda.
+                (Optional) The us-east-1 region AWS KMS customer managed key ARN for encrypting the repo access Github token AWS secret.
+                The key is created in the shared service account.
+                Required if var.use_repo_access_github_token is true.
                 EOT
-}
-
-variable "aws_organization_id" {
-  type         = string
-  description  = "(Required) The AWS organization ID."
+  default     = null
 }
